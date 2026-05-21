4f9390b65cc21ce21c67e39e0c63b939c7473e92
adisingh0824 <sadityasingh7990@gmail.com>
2026-05-21 23:52:55 +0530

    Commit

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE = 'ruh-imperium-static-v6';
+const STATIC_CACHE = 'ruh-imperium-static-v7';
 const RUNTIME_CACHE = 'ruh-imperium-runtime-v4';
 const OFFLINE_URL = '/offline.html';
 
